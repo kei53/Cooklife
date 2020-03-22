f75d8e72f30d51e634bdae89a993ca9501e76444
kei53 <kaiji303@gmail.com>
2020-03-22 16:09:06 +0900

    edit-show

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'b489135dc8ade2cdc58b89bef330f97cf95794f6a9620758521e2fb7532aeaa30b38bcbd43d22eb31856efdf616014959fa6a42b985638fe2f756de7648feb32'
